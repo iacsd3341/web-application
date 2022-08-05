6a5de7a75d75c6d2321239aa7937cca2ef17cd96
ShuHaRi <iacsd.33.41@gmail.com>
2022-08-05 03:21:46 -0400

    Project folder pushed successfully

--- Project/app.py
+++ Project/app.py
@@ -1,19 +1,79 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, migrate
 
 app = Flask(__name__)
+app.debug = True
+
+db_name='site.db'
+
+# adding configuration for using a sqlite database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Creating an SQLAlchemy instance
+db = SQLAlchemy(app)
+# Settings for migrations
+migrate = Migrate(app, db)
+
+# Models
+class Profile(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	first_name = db.Column(db.String(20), unique=False, nullable=False)
+	last_name = db.Column(db.String(20), unique=False, nullable=False)
+	age = db.Column(db.Integer, nullable=False)
+	def __repr__(self):
+	       	return f"Name : {self.first_name}, Age: {self.age}"
 
 
 
+# function to render index page
 @app.route('/')
+def index():
+        profiles = Profile.query.all()
+        return render_template('index.html',profiles=profiles)
 
-def home():
+@app.route('/add_data')
+def add_data():
+        return render_template('add_profile.html')
 
-        return render_template('home.html');
 
+
+
+# function to add profiles
+@app.route('/add', methods=["POST"])
+def profile():
+
+	# In this function we will input data from the
+	# form page and store it in our database.
+	# Remember that inside the get the name should
+	# exactly be the same as that in the html
+	# input fields
+	first_name = request.form.get("first_name")
+	last_name = request.form.get("last_name")
+	age = request.form.get("age")
+
+	# create an object of the Profile class of models
+	# and store data as a row in our datatable
+	if first_name != '' and last_name != '' and age is not None:
+		p = Profile(first_name=first_name, last_name=last_name, age=age)
+		db.session.add(p)
+		db.session.commit()
+		return redirect('/')
+	else:
+		return redirect('/')
+
+@app.route('/delete/<int:id>')
+def erase(id):
+    # deletes the data on the basis of unique id and
+    # directs to home page
+	data = Profile.query.get(id)
+	db.session.delete(data)
+	db.session.commit()
+	return redirect('/')
 
 
 if __name__ == '__main__':
 
-        app.run(host='0.0.0.0',port=6001)
+        app.run(host='0.0.0.0')
 
 
